666091562a43ebf674ac7a1ef66b6fd7d1785f76
Rujikorn Khampho <mr.rujikorn.khampho@gmail.com>
2022-07-31 11:16:46 +0700

    refactor: add blank and null parameters

--- backend/core/apps/product/models.py
+++ backend/core/apps/product/models.py
@@ -1,7 +1,6 @@
 from decimal import Decimal
 
-from core.apps.authentication.models import User
-from core.apps.product.choices import CoverType
+from core.apps.product.choices import BookCoverType, BookStyle
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
@@ -15,21 +14,22 @@ class BookCategory(models.Model):
 
 class Book(models.Model):
     category = models.ForeignKey(to=BookCategory, on_delete=models.CASCADE, related_name='books')
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=150)
     author = models.CharField(max_length=50)
-    cover_type = models.CharField(max_length=9, choices=CoverType.choices)
+    cover_type = models.CharField(max_length=9, choices=BookCoverType.choices, blank=True)
     normal_price = models.DecimalField(max_digits=7, decimal_places=2)
     percentage_discount = models.PositiveSmallIntegerField(
         validators=[MinValueValidator(limit_value=0), MaxValueValidator(limit_value=100)]
     )
-    total_quantity = models.PositiveSmallIntegerField()
+    total_quantity = models.PositiveSmallIntegerField(blank=True, null=True)
     barcode = models.CharField(max_length=13)
-    isbn = models.CharField(max_length=13)
-    year_of_print = models.CharField(max_length=50)
-    size = models.CharField(max_length=50)
-    number_of_page = models.CharField(max_length=50)
-    detail = models.TextField(null=True)
+    isbn = models.CharField(max_length=13, blank=True)
+    year_of_print = models.CharField(max_length=50, blank=True)
+    size = models.CharField(max_length=50, blank=True)
+    number_of_page = models.CharField(max_length=50, blank=True)
+    detail = models.TextField(blank=True)
     image = models.ImageField(upload_to='book/image/%Y-%m-%d')
+    style = models.CharField(max_length=10, choices=BookStyle.choices)
 
     @property
     def special_price(self):
