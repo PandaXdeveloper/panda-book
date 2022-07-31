d2061511a6379e7c82196ced87a949b326c9e367
Rujikorn Khampho <mr.rujikorn.khampho@gmail.com>
2022-07-31 09:32:44 +0700

    feat: add the api-auth into urlpatterns

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -20,5 +20,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('panda-book/api/', include('core.api_urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
